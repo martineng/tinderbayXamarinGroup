3935f4371c594ad6c0462c4c47f5b7d8f46ed772
martineng <Martin.ECA.6651@gmail.com>
2017-11-14 13:43:10 +0800

    Added Callum's part

--- TinderBay/Resources/Resource.designer.cs
+++ TinderBay/Resources/Resource.designer.cs
@@ -48,13 +48,25 @@ namespace TinderBay
 			public const int bike = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int car = 2130837505;
+			public const int bucket1 = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int fryingpan = 2130837506;
+			public const int car = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int userLoginEtxtBackground = 2130837507;
+			public const int fryingpan = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int Icon = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int Pencil = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int shoe1 = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int userLoginEtxtBackground = 2130837511;
 			
 			static Drawable()
 			{
@@ -69,95 +81,182 @@ namespace TinderBay
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060016
-			public const int TagSelectSpinner = 2131099670;
+			// aapt resource value: 0x7f060033
+			public const int TagSelectSpinner = 2131099699;
 			
-			// aapt resource value: 0x7f060017
-			public const int TagText = 2131099671;
+			// aapt resource value: 0x7f060034
+			public const int TagText = 2131099700;
 			
-			// aapt resource value: 0x7f06001d
-			public const int btnConfirm = 2131099677;
+			// aapt resource value: 0x7f06003a
+			public const int btnConfirm = 2131099706;
 			
-			// aapt resource value: 0x7f060012
-			public const int btnHistory = 2131099666;
+			// aapt resource value: 0x7f06002f
+			public const int btnHistory = 2131099695;
 			
-			// aapt resource value: 0x7f06000b
-			public const int btnLogin = 2131099659;
+			// aapt resource value: 0x7f060028
+			public const int btnLogin = 2131099688;
 			
-			// aapt resource value: 0x7f060014
-			public const int btnLogout = 2131099668;
+			// aapt resource value: 0x7f060031
+			public const int btnLogout = 2131099697;
 			
-			// aapt resource value: 0x7f060013
-			public const int btnSale = 2131099667;
+			// aapt resource value: 0x7f060030
+			public const int btnSale = 2131099696;
 			
-			// aapt resource value: 0x7f06000c
-			public const int btnSignup = 2131099660;
+			// aapt resource value: 0x7f060029
+			public const int btnSignup = 2131099689;
 			
-			// aapt resource value: 0x7f060011
-			public const int btnUpdateEmail = 2131099665;
+			// aapt resource value: 0x7f06002e
+			public const int btnUpdateEmail = 2131099694;
 			
-			// aapt resource value: 0x7f060010
-			public const int btnUpdatePassword = 2131099664;
+			// aapt resource value: 0x7f06002d
+			public const int btnUpdatePassword = 2131099693;
+			
+			// aapt resource value: 0x7f06001f
+			public const int button4 = 2131099679;
 			
 			// aapt resource value: 0x7f060000
 			public const int buybutton = 2131099648;
 			
-			// aapt resource value: 0x7f060019
-			public const int displayText = 2131099673;
+			// aapt resource value: 0x7f060036
+			public const int displayText = 2131099702;
 			
-			// aapt resource value: 0x7f06001c
-			public const int etxtConfirmPassword = 2131099676;
+			// aapt resource value: 0x7f060039
+			public const int etxtConfirmPassword = 2131099705;
 			
-			// aapt resource value: 0x7f06000a
-			public const int etxtPassword = 2131099658;
+			// aapt resource value: 0x7f060027
+			public const int etxtPassword = 2131099687;
 			
-			// aapt resource value: 0x7f060008
-			public const int etxtUsername = 2131099656;
-			
-			// aapt resource value: 0x7f060002
-			public const int imageView1 = 2131099650;
-			
-			// aapt resource value: 0x7f06000d
-			public const int imgbtnProfile = 2131099661;
-			
-			// aapt resource value: 0x7f060003
-			public const int linearLayout1 = 2131099651;
+			// aapt resource value: 0x7f060025
+			public const int etxtUsername = 2131099685;
 			
 			// aapt resource value: 0x7f060005
-			public const int linearLayout2 = 2131099653;
+			public const int gridLayout1 = 2131099653;
 			
-			// aapt resource value: 0x7f060004
-			public const int priceView = 2131099652;
+			// aapt resource value: 0x7f060020
+			public const int imageView1 = 2131099680;
+			
+			// aapt resource value: 0x7f06002a
+			public const int imgbtnProfile = 2131099690;
+			
+			// aapt resource value: 0x7f060002
+			public const int linearLayout1 = 2131099650;
+			
+			// aapt resource value: 0x7f060015
+			public const int linearLayout10 = 2131099669;
+			
+			// aapt resource value: 0x7f060018
+			public const int linearLayout11 = 2131099672;
+			
+			// aapt resource value: 0x7f06001b
+			public const int linearLayout12 = 2131099675;
+			
+			// aapt resource value: 0x7f06001c
+			public const int linearLayout13 = 2131099676;
+			
+			// aapt resource value: 0x7f060022
+			public const int linearLayout2 = 2131099682;
+			
+			// aapt resource value: 0x7f060006
+			public const int linearLayout4 = 2131099654;
+			
+			// aapt resource value: 0x7f060009
+			public const int linearLayout5 = 2131099657;
+			
+			// aapt resource value: 0x7f06000c
+			public const int linearLayout6 = 2131099660;
+			
+			// aapt resource value: 0x7f06000f
+			public const int linearLayout7 = 2131099663;
+			
+			// aapt resource value: 0x7f060012
+			public const int linearLayout8 = 2131099666;
+			
+			// aapt resource value: 0x7f060021
+			public const int priceView = 2131099681;
 			
 			// aapt resource value: 0x7f060001
 			public const int relativeLayout1 = 2131099649;
 			
-			// aapt resource value: 0x7f060018
-			public const int returnBtn = 2131099672;
+			// aapt resource value: 0x7f060035
+			public const int returnBtn = 2131099701;
 			
-			// aapt resource value: 0x7f060015
-			public const int searchEditText = 2131099669;
+			// aapt resource value: 0x7f060032
+			public const int searchEditText = 2131099698;
 			
-			// aapt resource value: 0x7f06001b
-			public const int vtxtConfirmPassword = 2131099675;
+			// aapt resource value: 0x7f060008
+			public const int textView12 = 2131099656;
+			
+			// aapt resource value: 0x7f06000b
+			public const int textView13 = 2131099659;
 			
 			// aapt resource value: 0x7f06000e
-			public const int vtxtEmail = 2131099662;
+			public const int textView14 = 2131099662;
 			
-			// aapt resource value: 0x7f06000f
-			public const int vtxtItemSale = 2131099663;
+			// aapt resource value: 0x7f060011
+			public const int textView15 = 2131099665;
 			
-			// aapt resource value: 0x7f060006
-			public const int vtxtLogin = 2131099654;
+			// aapt resource value: 0x7f060014
+			public const int textView16 = 2131099668;
 			
-			// aapt resource value: 0x7f060009
-			public const int vtxtPassword = 2131099657;
+			// aapt resource value: 0x7f060017
+			public const int textView17 = 2131099671;
 			
 			// aapt resource value: 0x7f06001a
-			public const int vtxtSignup = 2131099674;
+			public const int textView18 = 2131099674;
+			
+			// aapt resource value: 0x7f06001e
+			public const int textView19 = 2131099678;
+			
+			// aapt resource value: 0x7f060003
+			public const int textView4 = 2131099651;
+			
+			// aapt resource value: 0x7f060016
+			public const int view10 = 2131099670;
+			
+			// aapt resource value: 0x7f060019
+			public const int view11 = 2131099673;
+			
+			// aapt resource value: 0x7f06001d
+			public const int view12 = 2131099677;
+			
+			// aapt resource value: 0x7f060004
+			public const int view2 = 2131099652;
 			
 			// aapt resource value: 0x7f060007
-			public const int vtxtUsername = 2131099655;
+			public const int view5 = 2131099655;
+			
+			// aapt resource value: 0x7f06000a
+			public const int view6 = 2131099658;
+			
+			// aapt resource value: 0x7f06000d
+			public const int view7 = 2131099661;
+			
+			// aapt resource value: 0x7f060010
+			public const int view8 = 2131099664;
+			
+			// aapt resource value: 0x7f060013
+			public const int view9 = 2131099667;
+			
+			// aapt resource value: 0x7f060038
+			public const int vtxtConfirmPassword = 2131099704;
+			
+			// aapt resource value: 0x7f06002b
+			public const int vtxtEmail = 2131099691;
+			
+			// aapt resource value: 0x7f06002c
+			public const int vtxtItemSale = 2131099692;
+			
+			// aapt resource value: 0x7f060023
+			public const int vtxtLogin = 2131099683;
+			
+			// aapt resource value: 0x7f060026
+			public const int vtxtPassword = 2131099686;
+			
+			// aapt resource value: 0x7f060037
+			public const int vtxtSignup = 2131099703;
+			
+			// aapt resource value: 0x7f060024
+			public const int vtxtUsername = 2131099684;
 			
 			static Id()
 			{
@@ -176,25 +275,28 @@ namespace TinderBay
 			public const int CheckoutLayout = 2130968576;
 			
 			// aapt resource value: 0x7f040001
-			public const int HomeLayout = 2130968577;
+			public const int HistoryLayout = 2130968577;
 			
 			// aapt resource value: 0x7f040002
-			public const int Main = 2130968578;
+			public const int HomeLayout = 2130968578;
 			
 			// aapt resource value: 0x7f040003
-			public const int ProfileLayout = 2130968579;
+			public const int Main = 2130968579;
 			
 			// aapt resource value: 0x7f040004
-			public const int SearchLayout = 2130968580;
+			public const int ProfileLayout = 2130968580;
 			
 			// aapt resource value: 0x7f040005
-			public const int SearchResultLayout = 2130968581;
+			public const int SearchLayout = 2130968581;
 			
 			// aapt resource value: 0x7f040006
-			public const int SignupLayout = 2130968582;
+			public const int SearchResultLayout = 2130968582;
 			
 			// aapt resource value: 0x7f040007
-			public const int UpdatePasswordLayout = 2130968583;
+			public const int SignupLayout = 2130968583;
+			
+			// aapt resource value: 0x7f040008
+			public const int UpdatePasswordLayout = 2130968584;
 			
 			static Layout()
 			{
